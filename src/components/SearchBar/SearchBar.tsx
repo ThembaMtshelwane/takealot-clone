0040b9e56db76e579ad99762e7eb5659c73e0b44
Themba Mtshelwane <Thembamm3@gmail.com>
2023-04-26 12:12:47 +0200

    Searchbar -v2

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,6 +1,8 @@
-import { Box, Button,Flex,Input,List,Select,Spacer,Text} from '@chakra-ui/react';
+import { Box, Button,Flex,Input,InputGroup,InputLeftElement,InputRightAddon,List,Select,Spacer,Text} from '@chakra-ui/react';
 import React from 'react';
 import { useState } from 'react'; 
+import ShopByDepartment from './ShopByDepartment';
+import { Search2Icon } from '@chakra-ui/icons';
 
 type SearchBarProps = {
     
@@ -9,39 +11,22 @@ type SearchBarProps = {
 const SearchBar:React.FC<SearchBarProps> = () => {
     
     return(
-        <Flex bg={'#0b79bf'} height='99.48px' direction='row' align='center'>
-             <Box p='9px 64px 9px 15px'>
-               <Button  bg='#4d4d4f' color='white' height='32px' width='200px'
-                _active={{
-                  transform: 'scale(0.98)',
-                }}
-                _hover={{
-                  boxShadow:'0 0 1px 2px rgba(97, 143, 217, .75), 0 1px 1px rgba(0, 0, 0, .15)',
-                  bg: 'gray.700',
-                  color:'white' 
-                }}
-                >
-                  Shop by department
-                </Button>                   
-            </Box>
-            <Box p='8px 8px 8px 13px' width='90vw'>
-                <form action="">
-                    <Flex >
-                    <Input type="text"height='30px' bg='white'borderRadius='5px 0 0 5px' color='black' placeholder='Search for products, brands ...'/>
-                    <Button type='submit' height='30px' width='50px' bg='#4d4d4f' mr={5} borderRadius='0 5px 5px 0' color='white'
-                        _active={{
-                           transform: 'scale(0.98)',
-                         }}
-                         _hover={{
-                           boxShadow:'0 0 1px 2px rgba(97, 143, 217, .75), 0 1px 1px rgba(0, 0, 0, .15)',
-                           bg: 'gray.700',
-                           color:'white' 
-                         }}
-                    >Search </Button>
-                    </Flex>
-                </form>
-            </Box>
-        </Flex>
+      <Flex bg={'#0b79bf'} height='99.48px' direction='row' align='center'>
+       
+        <Box p='9px 64px 9px 15px'>
+          <ShopByDepartment/>               
+        </Box>
+        
+        <InputGroup borderRadius={5} size="sm" bg={'white'} width='75vw'> 
+          <Input type="text" placeholder="Search..." border="1px solid #949494" />
+          <InputRightAddon  p={0}  border="none"  >
+            <Button size="sm" borderLeftRadius={0} borderRightRadius={3.3} border="1px solid #949494"  bg='#4d4d4f'>
+            {<Search2Icon color="gray.50"/>}
+            </Button>
+         </InputRightAddon>
+        </InputGroup>
+    
+      </Flex>
     )
 }
 export default SearchBar;
---
+++ src/components/SearchBar/ShopByDepartment.tsx
@@ -0,0 +1,45 @@
+import { ChevronDownIcon } from '@chakra-ui/icons';
+import {
+    Menu,
+    MenuButton,
+    MenuList,
+    MenuItem,
+    MenuItemOption,
+    MenuGroup,
+    MenuOptionGroup,
+    MenuDivider,
+    Button,
+  } from '@chakra-ui/react'
+
+type ShopByDepartmentProps = {
+    
+};
+
+const ShopByDepartment:React.FC<ShopByDepartmentProps> = () => {
+    
+    return(
+     <Menu>
+        <MenuButton borderRadius={0} as={Button} rightIcon={<ChevronDownIcon />}
+        bg='#4d4d4f' color='white' height='32px' width='200px'
+          _active={{
+            transform: 'scale(0.98)',
+          }}
+          _hover={{
+            boxShadow:'0 0 1px 2px rgba(97, 143, 217, .75), 0 1px 1px rgba(0, 0, 0, .15)',
+            bg: 'gray.700',
+            color:'white' 
+          }}
+        >
+          Shop by Department
+        </MenuButton>
+        <MenuList>
+          <MenuItem>Download</MenuItem>
+          <MenuItem>Create a Copy</MenuItem>
+          <MenuItem>Mark as Draft</MenuItem>
+          <MenuItem>Delete</MenuItem>
+          <MenuItem>Attend a Workshop</MenuItem>
+        </MenuList>
+     </Menu>
+    )
+}
+export default ShopByDepartment;
