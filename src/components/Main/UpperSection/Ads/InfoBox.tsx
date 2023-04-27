a5f57f8ea0df23f21e3151987252825d6e53ed68
Themba Mtshelwane <Thembamm3@gmail.com>
2023-04-27 19:19:54 +0200

    InfoBox carousel

--- src/components/Main/UpperSection/Ads/InfoBox.tsx
+++ src/components/Main/UpperSection/Ads/InfoBox.tsx
@@ -1,15 +1,39 @@
-import { Flex } from '@chakra-ui/react';
-import React from 'react';
+import { ChevronLeftIcon, ChevronRightIcon } from '@chakra-ui/icons';
+import { Button, Flex, Icon, Spacer,Image } from '@chakra-ui/react';
+import React, { useState } from 'react';
+import image1 from '../../../../../public/Ads/InfoBox/image1.jpg'
+import image2 from '../../../../../public/Ads/InfoBox/image2.jpg'
+import image3 from '../../../../../public/Ads/InfoBox/image3.jpg'
+import image4 from '../../../../../public/Ads/InfoBox/image4.jpg'
 
 type InfoBoxProps = {
     
 };
 
 const InfoBox:React.FC<InfoBoxProps> = () => {
+    const images = [image1,image2,image3,image4]
+    const images2 = ['image1','image2','image3','image4']
+    const [imageCount, setImageCount] = useState(0)
+   
+    const clickRight = ()=>{
+        setImageCount(imageCount+1)
+        if(imageCount>=3){ setImageCount(0) }
+        return imageCount
+    }
+
+    const clickLeft = ()=>{
+        setImageCount(imageCount-1)
+        if(imageCount<1){ setImageCount(3) }
+        return imageCount
+    }
     
     return(
-        <Flex border='2px solid red' width='100%' p={5} direction='column'>
-            InfoBox
+        <Flex border='2px solid red' width='100%' p={5} direction='row'>
+            <Icon as={ChevronLeftIcon} onClick={clickLeft} borderRadius='100%'/>
+            {<p> {images2[imageCount]}</p> }
+            {/* <Image src={images2[imageCount]} alt='Dan Abramov' /> */}
+            <Spacer/>
+            <Icon as={ChevronRightIcon} onClick={clickRight} borderRadius='100%'/>
          </Flex>
     )
 }
