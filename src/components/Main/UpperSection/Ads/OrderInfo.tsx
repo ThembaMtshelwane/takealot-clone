0ab24df194529a42c8200c8cac3998378f65442f
Themba Mtshelwane <Thembamm3@gmail.com>
2023-04-27 18:15:07 +0200

    Order Info -v1

--- src/components/Main/UpperSection/Ads/OrderInfo.tsx
+++ src/components/Main/UpperSection/Ads/OrderInfo.tsx
@@ -1,4 +1,4 @@
-import { Flex } from '@chakra-ui/react';
+import { Box, Flex,Text, Image} from '@chakra-ui/react';
 import React from 'react';
 
 type OrderInfoProps = {
@@ -9,7 +9,13 @@ const OrderInfo:React.FC<OrderInfoProps> = () => {
     
     return(
         <>
-             OrderInfo
+        <Flex p={5} m={1} boxShadow='md' bg='gray.100' border='1.5px solid gray'>
+            <Image src='where-is-my-order.png' alt='where is my order' />
+            <Box>
+                <Text fontWeight={500} fontSize='15px'>Where's my order </Text>
+                <Text fontWeight={400} fontSize='12px'>Check your delivery or collection status</Text>
+            </Box>
+        </Flex>
         </>
     )
 }
