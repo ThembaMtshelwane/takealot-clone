60be874c92c042757f4f54f34d56f78343e7763e
Themba Mtshelwane <Thembamm3@gmail.com>
2023-04-27 21:47:40 +0200

    Cart-v2 Shopping cart image added

--- src/pages/cart/index.tsx
+++ src/pages/cart/index.tsx
@@ -1,4 +1,4 @@
-import { Box,Button,Flex,Text } from '@chakra-ui/react';
+import { Box,Button,Flex,Text,Image } from '@chakra-ui/react';
 import React from 'react';
 
 type CartProps = {
@@ -12,7 +12,7 @@ const Cart:React.FC<CartProps> = () => {
         <Text fontSize={20} fontWeight={700} color=' #4d4d4f'>Shopping Cart</Text>
         <Box bg='white' borderRadius='3px' boxShadow=' 0 2px 2px 0 rgba(77,77,79,0.08), 0 0 2px 0 rgba(77,77,79,0.16)' height='350px' mb='10px'>
             <Flex direction='column' align='center' m='24px 0'>
-                <Box h='120px' w='120px' border='2px solid blue' m='24px'></Box>
+            <Image mt={50} src="https://shopfront.takealot.com/1dcae9d1083903c04a3a0545f21483c835c8ceed/static/media/src/images/cart/empty-cart.svg-a3f63604a3d49f7b220a.svg" alt="Empty shopping cart" data-ref="empty-box-image" borderRadius='50%'/>
                 <Text m='8px 0 24px' color='#0a0a0a' fontSize={16} fontWeight={500} fontFamily='"Helvetica Neue",Helvetica,Roboto,Arial,sans-serif'>Your shopping cart is empty</Text>
                 <Button bg='#0b79bf' color='white' p='13px 32px' fontSize={14} borderRadius='3px'
                   _active={{
