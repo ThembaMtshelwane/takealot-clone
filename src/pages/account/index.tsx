2efd34f90b06d419dcc4d8fe5e0663179658d2e4
Themba Mtshelwane <Thembamm3@gmail.com>
2023-04-28 00:48:09 +0200

    My Accounts page -v1

--- src/pages/account/index.tsx
+++ src/pages/account/index.tsx
@@ -1,3 +1,4 @@
+import { Grid, GridItem,Text,Box, List, ListItem, Link, Stack,Image, Flex, Spacer } from '@chakra-ui/react';
 import React from 'react';
 
 type indexProps = {
@@ -6,6 +7,97 @@ type indexProps = {
 
 const index:React.FC<indexProps> = () => {
     
-    return <div>My Account</div>
+    return (
+        <>                
+            <Text fontSize='20px' fontWeight={700} pl='10px' pt='10px' color='#4d4d4f'>My Account</Text>
+            <Grid templateColumns='repeat(3,1fr)' p='20px 0'>
+                <GridItem>
+                    <Box margin='10px'  p='19px 24px 24px' bg='white' height='185px'>
+                        <Flex>
+                             <Text fontSize='16px' fontWeight={700} mb={2}>Orders</Text>
+                             <Spacer/>
+                             <Image src="https://img.icons8.com/external-anggara-glyph-anggara-putra/20/000000/external-cart-retail-anggara-glyph-anggara-putra-4.png" alt='cart'/>
+                        </Flex>
+                        <List color='#0b79bf' fontSize='14px'>
+                            <Stack spacing={2}>
+                                <Link href='/account/orders'><ListItem>Orders</ListItem></Link>
+                                <Link href='/account/invoices'><ListItem>Invoices</ListItem></Link>
+                                <Link href='/returns'><ListItem>Returns</ListItem></Link>
+                                <Link href='/account/reviews'><ListItem>Product Reviews</ListItem></Link>      
+                            </Stack>
+                        </List>
+                    </Box>
+                </GridItem>
+
+                <GridItem>
+                    <Box margin='10px'  p='19px 24px 24px' bg='white' height='185px'>
+                        <Flex>
+                             <Text fontSize='16px' fontWeight={700} mb={2}>Payments & Credit</Text>
+                             <Spacer/>
+                             <Image src="https://img.icons8.com/external-anggara-glyph-anggara-putra/20/000000/external-cart-retail-anggara-glyph-anggara-putra-4.png" alt='cart'/>
+                        </Flex>
+                        <List color='#0b79bf' fontSize='14px'>
+                            <Stack spacing={2}>
+                                <Link href='/account/credit'><ListItem>Credit & Refunds</ListItem></Link>
+                                <Link href='/voucher'><ListItem>Redeem Gift Voucher</ListItem></Link>    
+                            </Stack>
+                        </List>
+                    </Box>
+                </GridItem>
+
+                <GridItem>
+                    <Box margin='10px'  p='19px 24px 24px' bg='white' height='185px'>
+                        <Flex>
+                             <Text fontSize='16px' fontWeight={700} mb={2}>Customer Information</Text>
+                             <Spacer/>
+                             <Image src="https://img.icons8.com/external-anggara-glyph-anggara-putra/20/000000/external-cart-retail-anggara-glyph-anggara-putra-4.png" alt='cart'/>
+                        </Flex>
+                        <List color='#0b79bf' fontSize='14px'>
+                            <Stack spacing={2}>
+                                <Link href='/account/personal-details'><ListItem>Personal Details</ListItem></Link>
+                                <Link href='/address-book'><ListItem>Address Book</ListItem></Link>
+                                <Link href='/newsletter'><ListItem>Newsletter Subscriptions</ListItem></Link>     
+                            </Stack>
+                        </List>
+                    </Box>
+                </GridItem>
+
+                <GridItem>
+                    <Box margin='10px'  p='19px 24px 24px' bg='white' height='185px'>
+                        <Flex>
+                             <Text fontSize='16px' fontWeight={700} mb={2}>My Lists</Text>
+                             <Spacer/>
+                             <Image src="https://img.icons8.com/external-anggara-glyph-anggara-putra/20/000000/external-cart-retail-anggara-glyph-anggara-putra-4.png" alt='cart'/>
+                        </Flex>
+                        <List color='#0b79bf' fontSize='14px'>
+                            <Stack spacing={2}>
+                                <Link href='/wishlists'><ListItem>My Lists</ListItem></Link>
+                                <Link href='/wishlists/create-list'><ListItem>Create a List</ListItem></Link>   
+                            </Stack>
+                        </List>
+                    </Box>
+                </GridItem>
+
+                <GridItem>
+                    <Box margin='10px'  p='19px 24px 24px' bg='white' height='185px'>
+                        <Flex>
+                             <Text fontSize='16px' fontWeight={700} mb={2}>Orders</Text>
+                             <Spacer/>
+                             <Image src="https://img.icons8.com/external-anggara-glyph-anggara-putra/20/000000/external-cart-retail-anggara-glyph-anggara-putra-4.png" alt='cart'/>
+                        </Flex>
+                        <List color='#0b79bf' fontSize='14px'>
+                            <Stack spacing={2}>
+                                <Link href='/account/orders'><ListItem>Orders</ListItem></Link>
+                                <Link href='/account/invoices'><ListItem>Invoices</ListItem></Link>
+                                <Link href='/returns'><ListItem>Returns</ListItem></Link>
+                                <Link href='/account/reviews'><ListItem>Product Reviews</ListItem></Link>      
+                            </Stack>
+                        </List>
+                    </Box>
+                </GridItem>
+            </Grid>
+        
+        </>
+    )
 }
 export default index;
